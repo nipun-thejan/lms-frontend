869a8d8b0de2dcf8fb15eeb5812444509c32abe0
nipun thejan <76776959+nipun-thejan@users.noreply.github.com>
2023-02-12 16:20:36 +0530

    init

---
+++ src/pages/Login.js
@@ -0,0 +1,81 @@
+import { useState, useEffect } from 'react';
+import { Logo, FormRow, Alert } from '../components';
+import Wrapper from '../assets/wrappers/RegisterPage';
+import { useAppContext } from '../context/appContext';
+import { useNavigate } from 'react-router-dom';
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+};
+
+const Login = () => {
+  const navigate = useNavigate();
+  const [values, setValues] = useState(initialState);
+  const { user, isLoading, showAlert, displayAlert, setupUser } =
+    useAppContext();
+
+  // const toggleMember = () => {
+  //   setValues({ ...values, isMember: !values.isMember });
+  // };
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const { name, email, password, isMember } = values;
+    if (!email || !password || (!isMember && !name)) {
+      displayAlert();
+      return;
+    }
+    const currentUser = { name, email, password };
+
+    setupUser({
+      currentUser,
+      endPoint: 'login',
+      alertText: 'Login Successful! Redirecting...',
+    });
+
+  };
+
+  useEffect(() => {
+    if (user) {
+      setTimeout(() => {
+        navigate('/');
+      }, 3000);
+    }
+  }, [user, navigate]);
+
+  return (
+    <Wrapper className='full-page'>
+      <form className='form' onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        {showAlert && <Alert />}
+        {/* name input */}
+
+        {/* email input */}
+        <FormRow
+          type='email'
+          name='email'
+          value={values.email}
+          handleChange={handleChange}
+        />
+        {/* password input */}
+        <FormRow
+          type='password'
+          name='password'
+          value={values.password}
+          handleChange={handleChange}
+        />
+        <button type='submit' className='btn btn-block' disabled={isLoading}>
+          submit
+        </button>
+
+      </form>
+    </Wrapper>
+  );
+};
+export default Login;
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Landing from './Landing'
+import Error from './Error'
+import Register from './Register'
+import ProtectedRoute from './ProtectedRoute'
+import Login from './Login'
+export { Landing, Error, Register, ProtectedRoute, Login }
